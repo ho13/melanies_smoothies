336f2a861338d7009410a333e92345712e8dd8ea
Ho Yi-Shan <51830341+ho13@users.noreply.github.com>
2025-01-03 09:52:48 +0000

    Update streamlit_app.py
    updating code as per L10-3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # -- Import python packages
 import streamlit as st
+import requests
 
 # -- import the function col so that we bring back only the column name at rather than the whole table at line 26 
 from snowflake.snowpark.functions import col
@@ -46,6 +47,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
     # st.write(ingredients_string) 
 
@@ -62,11 +65,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-# New section to display smoothiefroot nutrition information 
-import requests
-smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
 
 
