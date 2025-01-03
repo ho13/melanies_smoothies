87bfbb471f447d9cb5adc27e92e04313fcd30e90
Ho Yi-Shan <51830341+ho13@users.noreply.github.com>
2025-01-03 13:55:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # -- Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 import pandas as pd
 
 # -- import the function col so that we bring back only the column name at rather than the whole table at line 26 
